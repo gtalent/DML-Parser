4b40cd43c20c4f8832a46bfbe2289fe81ee01504
Gary Talent <gtalent2@gmail.com>
2010-11-30 20:14:23 -0600

    Added a link command to the Makefile.

--- Makefile
+++ Makefile
@@ -5,3 +5,6 @@ GOFILES=\
 	main.go\
 
 include $(GOROOT)/src/Make.pkg
+
+link:
+	6l -o parser _go_.6
--- main.go
+++ main.go
@@ -2,16 +2,23 @@ package main
 
 import(
     "fmt"
-    "dml"
     "flag"
+    "io/ioutil"
+    "dml"
 )
 
 func main() {
 	html := flag.Bool("embed", false, "Embeds the converted DML in an HTML body before writing it out.")
 	flag.Parse()
+
+	file, err := ioutil.ReadFile(flag.Arg(0))
+        if err != nil {
+		fmt.Println("Could not" + err.String())
+        }
+
 	if *html {
-		fmt.Println(dml.ToHTML(flag.Arg(0)))
+		fmt.Println(dml.ToHTML(flag.Arg(0), string(file)))
 	} else {
-		fmt.Println(dml.ParseDoc(flag.Arg(0)))
+		fmt.Println(dml.ParseDoc(string(file)))
 	}
 }
